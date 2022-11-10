8e34aeffa8a0c84b2fd2b8d1ed796b61784c7c7c
nom2 <nom2@email.com>
2022-11-10 17:54:57 +0100

    update stat

--- stat.py
+++ stat.py
@@ -1,13 +1,41 @@
+import random
 from tri_bulles import *
 from tribubulle import *
 from triinsertion import *
 from triselectif1 import *
 
 def stat(min:int,max:int,step:int,nbr:int):
-    for i in range(n):
-        for j in range(min, max-1):
-            if tab[j] > tab[j+1] :
-                tab[j], tab[j+1] = tab[j+1], tab[j]
-    print(tab)
+    tab = []
+    s = 0
+    for i in range(min,max):
+        tab.append(round(random.uniform(0,50), 2))
+    print("Tableau :",tab)
+    print("")
+    print("Tri à bulles :"), tri_bulle(tab)
+    print("Tri à bulles optimisé :") , bubulle(tab)
+    print("Tri à insertition :") , triinsertion(tab)
+    print("Tri à sélection :"), triselectif1(tab)
+    print("Stats :")
+    p = min
+    while s < len(tab):
+        print (p, tab[s])
+        if s == 0 and step!=1:
+            p=p+step
+            s=s+(step-1)
+        else:
+            p=p+step
+            s=s+step
 
-tri_bulle([1,3,1,4654654,456,789,2,3,9,8,6])
+stat(10, 20, 5, 10)
+
+"""
+Ecrire une méthode stat (int min, int max, int step, int nbr) qui fait varier la taille des tableaux tirés
+au sort de min jusqu’à max en avançant de step à chaque fois. Pour chaque taille, effectuez nbr
+générations aléatoires de tableaux et appelez la fonction de tri à tester. Indiquez sur la sortie
+standard la taille du tableau suivi du nombre moyen d’opérations effectués, avec une ligne pour
+chaque taille. Par exemple pour min = 10, max = 20, step = 5 et nbr = 10 on obtient quelque chose
+comme
+10 12.3
+15 27.4
+20 32.4
+"""
