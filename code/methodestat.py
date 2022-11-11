3942e49674a87c1bdfc72ecd68d200c20a4ab0be
Eduardo-Cerqueira <eduardoccerqueira778@gmail.com>
2022-11-11 19:01:47 +0100

    update stat for goog tab

--- code/methodestat.py
+++ code/methodestat.py
@@ -1,92 +1,54 @@
 import random
-from tri_bulles import *
+from tribulles import *
 from tribubulle import *
 from triinsertion import *
 from triselectif import *
 
 def stat(min:int,max:int,step:int,nbr:int):
     e = min
-    y = min
-    i = min
-    p = min
-    print("")
-    print("Bulle :")
     while e < max+1:
-        tabgenbulle(e, nbr)
+        tabgen(e, nbr)
         e += step
-    print("")
-    print("Bulle Opti :")
-    while y < max+1:
-        tabgenbulleopti(y, nbr)
-        y += step
-    print("")
-    print("Insertion :")
-    while i < max+1:
-        tabgeninsertion(i, nbr)
-        i += step
-    print("")
-    print("Selectif :")
-    while p < max+1:
-        tabgenselectif(p, nbr)
-        p += step
     
 
-def tabgenbulle(arraysize,nbr:int):
+def tabgen(arraysize,nbr:int):
     bulle = []
     totalbulle = 0
-    i = 0
-    for x in range(0,nbr):
-        tab = []
-        for y in range(0,arraysize):
-            tab.append(round(random.uniform(0,100)))
-        bulle.append(tri_bulle(tab))
-    while i < len(bulle):
-        totalbulle += bulle[i]
-        i += 1
-    totalbulle /= len(bulle)
-    print(arraysize,totalbulle)
-    
-def tabgenbulleopti(arraysize,nbr:int):
     bulleopti = []
     totalbulleopti = 0
-    i = 0
-    for x in range(0,nbr):
-        tab = []
-        for y in range(0,arraysize):
-            tab.append(round(random.uniform(0,100)))
-        bulleopti.append(bubulle(tab))
-    while i < len(bulleopti):
-        totalbulleopti += bulleopti[i]
-        i += 1
-    totalbulleopti /= len(bulleopti)
-    print(arraysize,totalbulleopti)
-
-def tabgeninsertion(arraysize,nbr:int):
+    selectif = []
+    totalselectif = 0
     insertion = []
     totalinsertion = 0
     i = 0
     for x in range(0,nbr):
         tab = []
+        tab1 = []
+        tab2 = []
+        tab3 = []
         for y in range(0,arraysize):
             tab.append(round(random.uniform(0,100)))
-        insertion.append(triinsertion(tab))
-    while i < len(insertion):
+            tab1.append(tab[y])
+            tab2.append(tab[y])
+            tab3.append(tab[y])
+        bulle.append(tri_bulle(tab))
+        bulleopti.append(bubulle(tab1))
+        insertion.append(triinsertion(tab2))
+        selectif.append(triselectif(tab3))
+    while i < len(bulle):
+        totalbulle += bulle[i]
+        totalbulleopti += bulleopti[i]
         totalinsertion += insertion[i]
-        i += 1
-    totalinsertion /= len(insertion)
-    print(arraysize,totalinsertion)
-
-def tabgenselectif(arraysize,nbr:int):
-    selectif = []
-    totalselectif = 0
-    i = 0
-    for x in range(0,nbr):
-        tab = []
-        for y in range(0,arraysize):
-            tab.append(round(random.uniform(0,100)))
-        selectif.append(triselectif(tab))
-    while i < len(selectif):
         totalselectif += selectif[i]
         i += 1
+    totalbulle /= len(bulle)
+    totalbulleopti /= len(bulleopti)
+    totalinsertion /= len(insertion)
     totalselectif /= len(selectif)
-    print(arraysize,totalselectif)
+    print("Tableau de",arraysize)
+    print("")
+    print("bulle :",totalbulle)
+    print("bulle opti :",totalbulleopti)
+    print("insertion :",totalinsertion)
+    print("selectif :",totalselectif)
+    print("")
